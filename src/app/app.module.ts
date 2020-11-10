5716352f264618172ddb7fee16e53ccc86a8d507
Szy György <szygyuri@gmail.com>
2020-11-10 17:31:59 +0100

    hero nagyjából jó

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { MatButtonModule } from "@angular/material/button";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FlexLayoutModule],
+  imports: [BrowserModule, FlexLayoutModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
