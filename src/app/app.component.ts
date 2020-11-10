fc2b21077a70f7d1e9f9f86eaecf2c652cbc9d9a
Szy György <szygyuri@gmail.com>
2020-11-10 21:22:39 +0100

    egyelőre csak a hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "tothozon.hu";
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: "smooth" });
+  }
 }
