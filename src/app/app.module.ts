a5830b5161070d9c1651b3bda12396a7c6d21713
Szy György <szygyuri@gmail.com>
2020-11-10 17:42:13 +0100

    nagyjából kirakni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FlexLayoutModule, MatButtonModule],
+  imports: [BrowserModule, FlexLayoutModule, MatButtonModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
